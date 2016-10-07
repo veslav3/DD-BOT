8f2c81624c44512d3d4cce23cc789c5d1a15ac61
Roy Honders <royhonders@gmail.com>
2016-10-07 22:50:41 +0200

    Fresh startup project5

---
+++ DD-Bot/DD-Bot/DDbot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DD_Bot
+{
+    class DDbot
+    {
+        DiscordClient discord;
+
+        public DDbot()
+        {
+            discord = new DiscordClient(clientLogger =>
+            {
+                clientLogger.LogLevel = LogSeverity.Info;
+                clientLogger.LogHandler = Log;
+            });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MjM0MDIwNDcwNzQyMTg4MDMz.Ctl8GA.IwmjhBOdLGhN1MjqKFiMAxI0Hhg");
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ DD-Bot/DD-Bot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DD_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DDbot bot = new DDbot();
+        }
+    }
+}
