4dc66e31344c21d463e3da00e4d98e380605b455
Roy Honders <royhonders@gmail.com>
2016-10-09 19:23:04 +0200

    channel is nu instelbaar.

--- Discordbot/DDbot.cs
+++ Discordbot/DDbot.cs
@@ -40,10 +40,10 @@ namespace Discordbot
                 Console.WriteLine("audio werkt!");
                 try
                 {
-                    var voiceChannel = discord.FindServers("SQUAD").FirstOrDefault().VoiceChannels.FirstOrDefault(); // Finds the first VoiceChannel on the server 'Music Bot Server'
+                    var voiceChannel = discord.FindServers("SQUAD").FirstOrDefault().VoiceChannels.ToArray(); // Finds the first VoiceChannel on the server 'Music Bot Server'
 
                     var _vClient = await discord.GetService<AudioService>() // We use GetService to find the AudioService that we installed earlier. In previous versions, this was equivelent to _client.Audio()
-                            .Join(voiceChannel); // Join the Voice Channel, and return the IAudioClient.
+                            .Join(voiceChannel[0]); // 1 = team 1, 2 = team 3, 3 = team 2 en 0 = General
                     var channelCount = discord.GetService<AudioService>().Config.Channels; // Get the number of AudioChannels our AudioService has been configured to use.
                     var OutFormat = new WaveFormat(48000, 16, channelCount); // Create a new Output Format, using the spec that Discord will accept, and with the number of channels that our client supports.
                     using (var MP3Reader = new Mp3FileReader("/DD-Bot/Discordbot/sounds/johncena.mp3")) // Create a new Disposable MP3FileReader, to read audio from the filePath parameter
